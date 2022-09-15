31a7109573b9b0b979ea44656f7b74d7e04b1d01
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-15 17:51:51 -0300

    feat: create card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+interface Props {
+  imageUrl: string;
+}
+
+export const Card = ({ imageUrl }: Props) => {
+  return (
+    <div className='w-[337px] h-[455px] bg-zinc-100 rounded-xl flex flex-col'>
+      <div className='relative w-full h-[13.1875rem]'>
+        <Image
+          src={imageUrl}
+          className='rounded-tl-xl rounded-tr-xl'
+          layout='fill'
+          objectFit='cover'
+        />
+      </div>
+
+      <div className='flex flex-col px-8 pt-6 pb-7 gap-4'>
+        <h2 className='text-sm font-semibold text-[#abadc6]'>April 17, 2022</h2>
+        <p className='text-lg font-semibold'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          consequat quam nibh.
+        </p>
+
+        <button className='w-full p-3 bg-blue-500 text-white text-sm font-semibold rounded-xl '>
+          Ver
+        </button>
+      </div>
+    </div>
+  );
+};
