aabc82c7eb9a87e6142a31a2c1eafc4df662397e
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-15 17:09:15 -0300

    feat: create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import { Logo } from './Logo';
+
+export const Navbar = () => {
+  return (
+    <nav className='bg-[#1E2235] w-full h-[68px] flex items-center p-5'>
+      <Logo />
+    </nav>
+  );
+};
