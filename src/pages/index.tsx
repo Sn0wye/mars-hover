c28808cbfe2701c01d1d667b068aca2d9ad0bdcc
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-16 16:04:25 -0300

    feat: add homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+import type { Photo } from '../@types';
 import { Card } from '../components/Card';
+import { Loading } from '../components/Loading';
 import { Navbar } from '../components/Navbar';
+import { fetchPhotos } from '../services/fetchPhotos';
+
+interface Data {
+  photos: Photo[];
+}
 
 export default function Home() {
+  const { data, isFetching } = useQuery<Data>(['teste'], fetchPhotos, {
+    refetchOnWindowFocus: false,
+  });
+
+  if (isFetching) {
+    return <Loading />;
+  }
+
   return (
     <>
       <Navbar />
       <main className='pt-16 px-6 flex flex-wrap gap-4'>
-        <Card imageUrl='/assets/gato.png' />
-        <Card imageUrl='/assets/gato.png' />
+        {data &&
+          data.photos.map((item) => (
+            <Card key={item.id} imageUrl={item.img_src} />
+          ))}
       </main>
     </>
   );
