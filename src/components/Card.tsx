1f852d8510cbd61702d4be31c1cdd1b424019ebd
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-16 16:04:19 -0300

    feat: add fullscreen feature to card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,54 @@
 import Image from 'next/image';
+import { useState } from 'react';
+import { FullscreenImage } from './FullscreenImage';
 
 interface Props {
   imageUrl: string;
 }
 
 export const Card = ({ imageUrl }: Props) => {
+  const [isImageFullscreen, setIsImageFullscreen] = useState(false);
+
+  const onToggleFullscreen = () => {
+    setIsImageFullscreen((state) => !state);
+  };
+
   return (
-    <div className='w-[337px] h-[455px] bg-zinc-100 rounded-xl flex flex-col'>
-      <div className='relative w-full h-[13.1875rem]'>
-        <Image
-          src={imageUrl}
-          className='rounded-tl-xl rounded-tr-xl'
-          layout='fill'
-          objectFit='cover'
+    <>
+      <div className='w-[337px] h-[455px] bg-zinc-100 rounded-xl flex flex-col'>
+        <div className='relative w-full h-[13.1875rem]'>
+          <Image
+            src={imageUrl}
+            className='rounded-tl-xl rounded-tr-xl'
+            layout='fill'
+            objectFit='cover'
+          />
+        </div>
+
+        <div className='flex flex-col px-8 pt-6 pb-7 gap-4'>
+          <h2 className='text-sm font-semibold text-[#abadc6]'>
+            April 17, 2022
+          </h2>
+          <p className='text-lg font-semibold'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            consequat quam nibh.
+          </p>
+
+          <button
+            className='w-full p-3 bg-blue-500 text-white text-sm font-semibold rounded-xl'
+            onClick={onToggleFullscreen}
+          >
+            Ver
+          </button>
+        </div>
+      </div>
+
+      {isImageFullscreen && (
+        <FullscreenImage
+          imageUrl={imageUrl}
+          onRequestExit={onToggleFullscreen}
         />
-      </div>
-
-      <div className='flex flex-col px-8 pt-6 pb-7 gap-4'>
-        <h2 className='text-sm font-semibold text-[#abadc6]'>April 17, 2022</h2>
-        <p className='text-lg font-semibold'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          consequat quam nibh.
-        </p>
-
-        <button className='w-full p-3 bg-blue-500 text-white text-sm font-semibold rounded-xl '>
-          Ver
-        </button>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
