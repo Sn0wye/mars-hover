84c803f75eed5bcba3e8b71c0aad896115a89414
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-16 16:13:37 -0300

    feat: change api URL

--- src/services/fetchPhotos.ts
+++ src/services/fetchPhotos.ts
@@ -1,6 +1,6 @@
 export const fetchPhotos = async () => {
   const res = await fetch(
-    'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=CXYqQiMgj2yFMLmGMIhaxiNmQbTABX6LLubi3nWU'
+    'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=CXYqQiMgj2yFMLmGMIhaxiNmQbTABX6LLubi3nWU'
   );
 
   const data = res.json();
