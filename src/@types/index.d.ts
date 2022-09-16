444a698ca5e5324f497b68cf7404920bbdad151c
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-09-16 15:14:33 -0300

    feat: separe types into a different file

---
+++ src/@types/index.d.ts
@@ -0,0 +1,21 @@
+export interface Rover {
+  id: number;
+  name: string;
+  landing_date: string;
+  launch_date: string;
+  status: string;
+}
+
+export interface Camera {
+  id: number;
+  name: string;
+  rover_id: number;
+  full_name: string;
+}
+
+export interface Photo {
+  id: number;
+  img_src: string;
+  camera: Camera;
+  rover: Rover;
+}
